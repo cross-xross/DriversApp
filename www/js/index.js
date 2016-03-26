fa2a84fde205e5ee5f112488023917bde1ad2cd7
cross-xross <to.kawashima0207@gmail.com>
2016-03-26 11:39:07 +0900

    refine user interface.

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,6 @@ var ViewModel = {
 	items: ko.observableArray([
 		{ startTime: "09:00", location: "渋谷", arrivalTime: "10:05" },
 		{ startTime: "10:21", location: "横浜", arrivalTime: "12:30" },
-		{ startTime: "13:05", location: "平塚", arrivalTime: "15:00" }
+		{ startTime: "13:05", location: "平塚", arrivalTime: "15:00" },
     ])
 };
