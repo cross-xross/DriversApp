b8e1e1fc8ac0402691b9c975be6411a50ee9029e
Ke Shimizu <aa@aa.com>
2016-04-03 11:13:21 +0900

    implement RecordButton

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,11 @@
 var ViewModel = {
 	items: ko.observableArray([
-		{ startTime: "09:00", location: "渋谷", arrivalTime: "10:05" },
-		{ startTime: "10:21", location: "横浜", arrivalTime: "12:30" },
-		{ startTime: "13:05", location: "平塚", arrivalTime: "15:00" },
-    ])
+		{ arrivalTime: "09:00", location: "渋谷", departureTime: "10:05" },
+		{ arrivalTime: "10:21", location: "横浜", departureTime: "12:30" },
+		{ arrivalTime: "13:05", location: "平塚", departureTime: "15:00" },
+    ]),
+    inputArrivalTime: ko.observable(""),
+    inputDepartureTime: ko.observable(""),
+    inputLocation: ko.observable("Sample"),
+    inputLocation2: ko.observable("Sample2")
 };
