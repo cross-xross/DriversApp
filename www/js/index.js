f9ce69736bc612c90292ea6f9f9546df4f07f383
Ke Shimizu <aa@aa.com>
2016-04-24 12:23:32 +0900

    implement RecordButton rev.4

--- www/js/index.js
+++ www/js/index.js
@@ -4,13 +4,20 @@ $(document).ready(function() {
     ko.applyBindings(ViewModel);
 });
 
-var current_id;    // getEntriedStatus()、editRow() 等で値設定する
+var current_id;    // 現在編集中の行番号。getEntriedStatus()、editRow() 等で値設定する
+
+// メイン画面 記録表の入力状態
+var EntriedColumnStatus = {
+    ArrivalTime: 0,     // 到着時刻が入力済
+    Location: 1,        // 経由地まで入力済
+    DepartureTime: 2    // 出発時刻まで入力済
+};
 
 var ViewModel = {
     items: ko.observableArray([
-        { arrivalTime: "09:00", location: "渋谷", departureTime: "10:05" },
-        { arrivalTime: "10:21", location: "横浜", departureTime: "12:30" },
-        { arrivalTime: "13:05", location: "平塚", departureTime: "15:00" },
+        { arrivalTime: "09:00", location: "渋谷", departureTime: "10:05", movingTime: "" },
+        { arrivalTime: "10:21", location: "横浜", departureTime: "12:30", movingTime: "0:16" },
+        { arrivalTime: "13:05", location: "平塚", departureTime: "15:00", movingTime: "0:35" },
     ]),
     inputArrivalTime: ko.observable(""),
     inputDepartureTime: ko.observable(""),
@@ -19,46 +26,49 @@ var ViewModel = {
     // 経由地編集ダイアログで登録ボタンタップ時の処理
     updateRow: function() {
         ViewModel.items.splice(current_id, 1,
-            { arrivalTime: ViewModel.inputArrivalTime(), location: ViewModel.inputLocation(), departureTime: ViewModel.inputDepartureTime() });
+            { arrivalTime: ViewModel.inputArrivalTime(), location: ViewModel.inputLocation(),
+            departureTime: ViewModel.inputDepartureTime(), movingTime: getMovingTime() });
         $.mobile.changePage('#main_screen');
     },
     // 経由地名称更新ダイアログで登録ボタンタップ時の処理
     updateLocation: function() {
         var rowdata = ViewModel.items()[current_id];
         ViewModel.items.splice(current_id, 1,
-            { arrivalTime: rowdata.arrivalTime, location: ViewModel.inputLocation2(), departureTime: rowdata.departureTime });
+            { arrivalTime: rowdata.arrivalTime, location: ViewModel.inputLocation2(),
+            departureTime: rowdata.departureTime, movingTime: rowdata.movingTime });
         $.mobile.changePage('#main_screen');
     },
-    // メイン画面記録ボタンタップ時の処理
+    // メイン画面 記録ボタンタップ時の処理
     record: function() {
         var status = getEntriedStatus();
-//alert('status:' + status + ', current_id:' + current_id);
         switch (status) {
-            case 0:
+            case EntriedColumnStatus.ArrivalTime:
                 $("#recordLocationDialog").popup("open");
                 break;
-            case 1:
+            case EntriedColumnStatus.Location:
                 //出発時刻入力
                 var rowdata = ViewModel.items()[current_id];
                 ViewModel.items.splice(current_id, 1,
-                    { arrivalTime: rowdata.arrivalTime, location: rowdata.location, departureTime: now() });
+                    { arrivalTime: rowdata.arrivalTime, location: rowdata.location, departureTime: now(), movingTime: rowdata.movingTime });
                 break;
-            case 2:
+            case EntriedColumnStatus.DepartureTime:
                 //到着時刻入力
-                ViewModel.items.push({ arrivalTime: now(), location: "", departureTime: "" });
+                ViewModel.items.push({ arrivalTime: now(), location: "", departureTime: "", movingTime: getMovingTime() });
                 break;
         }
+    },
+    // メイン画面 記録表の行タップ時の処理
+    editRow: function(rowdata, event) {
+        current_id = event.target.id;
+        ViewModel.inputArrivalTime(rowdata.arrivalTime);
+        ViewModel.inputLocation(rowdata.location);
+        ViewModel.inputDepartureTime(rowdata.departureTime);
+        $('#popupRegist').popup('open');
     }
 };
 
-function editRow(element) {
-    var row = element.id;
-    var data = ViewModel.items()[row];
-    ViewModel.inputArrivalTime(data.arrivalTime);
-    ViewModel.inputLocation(data.location);
-    ViewModel.inputDepartureTime(data.departureTime);
-    current_id = element.id;
-    $('#popupRegist').popup('open');
+function getMovingTime() {
+    return "AA:BB";
 }
 
 // 現在の入力済み状態を取得
@@ -69,16 +79,16 @@ function getEntriedStatus() {
         current_id = row;
         var rowdata = ViewModel.items()[row];
         if (rowdata.departureTime != "") {
-            //出発時刻まで入力済
-            return 2;
+            // 出発時刻まで入力済
+            return EntriedColumnStatus.DepartureTime;
         }
         if (rowdata.location != "") {
-            //経由地まで入力済
-            return 1;
+            // 経由地まで入力済
+            return EntriedColumnStatus.Location;
         }
         if (rowdata.arrivalTime != "") {
-            //到着時刻が入力済
-            return 0;
+            // 到着時刻が入力済
+            return EntriedColumnStatus.ArrivalTime;
         }
         row--;
     }
